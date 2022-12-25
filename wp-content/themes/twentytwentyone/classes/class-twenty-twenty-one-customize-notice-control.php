1e840b1bc4530a21614bf3e5e5786800e0429517
Rushikesh Walapkar <rushiwalapkar@gmail.com>
2022-12-26 01:02:06 +0530

    Delete wp-content directory

--- wp-content/themes/twentytwentyone/classes/class-twenty-twenty-one-customize-notice-control.php
+++
@@ -1,5 +0,0 @@
-<br>
-<b>Fatal error</b>:  Uncaught Error: Class 'WP_Customize_Control' not found in C:\xampp\htdocs\library\wp-content\themes\twentytwentyone\classes\class-twenty-twenty-one-customize-notice-control.php:17
-Stack trace:
-#0 {main}
-  thrown in <b>C:\xampp\htdocs\library\wp-content\themes\twentytwentyone\classes\class-twenty-twenty-one-customize-notice-control.php</b> on line <b>17</b><br>
--- wp-content/themes/twentytwentyone/functions.php
+++
@@ -1,7 +0,0 @@
-<br>
-<b>Notice</b>:  Undefined index: wp_version in <b>C:\xampp\htdocs\library\wp-content\themes\twentytwentyone\functions.php</b> on line <b>13</b><br>
-<br>
-<b>Fatal error</b>:  Uncaught Error: Call to undefined function get_template_directory() in C:\xampp\htdocs\library\wp-content\themes\twentytwentyone\functions.php:14
-Stack trace:
-#0 {main}
-  thrown in <b>C:\xampp\htdocs\library\wp-content\themes\twentytwentyone\functions.php</b> on line <b>14</b><br>
